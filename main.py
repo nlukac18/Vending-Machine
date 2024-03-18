ab587d6a4a84dcba38e5f35560d955a2ebdefd67
hbergcbrandon <163863122+hbergcbrandon@users.noreply.github.com>
2024-03-18 09:55:00 -0400

    Update Inventory Count
    Added functionality to update the count of each item in the machine after a purchase

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import inventory as inv
 import user_io as io
 def main():
   result = "Yes"
-  print("These are your choices:", inv.inventory_dict)
   money = io.get_money()
   while result == "Yes":  
+    print("These are your choices:", inv.inventory_dict)
     user_selection = io.get_choice(inv.inventory_dict)
     item_price = float(inv.inventory_dict[user_selection][0])
     if item_price > money:
@@ -14,10 +14,12 @@ def main():
         print(f'Here is your {user_selection} no change is required')
         money = money - item_price
         result = input("Would you like anything else: Yes/No ")
+        inv.inventory_dict[user_selection][1] = inv.inventory_dict[user_selection][1] - 1
     else:
         change = money - item_price
         print(f'Here is your {user_selection} and your change ${change}')
         money = money - item_price
+        inv.inventory_dict[user_selection][1] = inv.inventory_dict[user_selection][1] - 1
         result = input("Would you like anything else: Yes/No ")  
 main()
 
