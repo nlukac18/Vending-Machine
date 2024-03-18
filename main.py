a9b6a9ce28b552d4712e7f0632c6e94a12b837c8
nlukac18 <nlukac@heidelberg.edu>
2024-03-18 09:36:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,22 @@
 import inventory as inv
 import user_io as io
 def main():
+  result = "Yes"
   print("These are your choices:", inv.inventory_dict)
   money = io.get_money()
-  user_selection = io.get_choice(inv.inventory_dict)
-  item_price = float(inv.inventory_dict[user_selection][0])
-  if item_price > money:
-    print("Error, insufficient funds")
-  elif item_price == money:
-    print(f'Here is your {user_selection} no change is required')
-  else:
-    change = money - item_price
-    print(f'Here is your {user_selection} and your change ${change}')
-    result = input("Would you like anything else: Yes/No ")
-  if result == "Yes":
-    main()
-  else:
-    print("Thank you for coming to the vending machine!")  
+  while result == "Yes":  
+    user_selection = io.get_choice(inv.inventory_dict)
+    item_price = float(inv.inventory_dict[user_selection][0])
+    if item_price > money:
+        print("Error, insufficient funds")
+    elif item_price == money:
+        print(f'Here is your {user_selection} no change is required')
+        money = money - item_price
+        result = input("Would you like anything else: Yes/No ")
+    else:
+        change = money - item_price
+        print(f'Here is your {user_selection} and your change ${change}')
+        money = money - item_price
+        result = input("Would you like anything else: Yes/No ")  
 main()
 
