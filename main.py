637d498b0641149dc132a5da177d21b4a5cbe8bf
nlukac18 <nlukac@heidelberg.edu>
2024-03-20 09:06:13 -0400

    commit

--- main.py
+++ main.py
@@ -3,21 +3,27 @@ import inventory as inv
 import user_io as io
 def main():
   result = "Yes"
-  print("These are your choices:", inv.inventory_dict)
   money = io.get_money()
   while result == "Yes":  
+    print("These are your choices:", inv.inventory_dict)
     user_selection = io.get_choice(inv.inventory_dict)
-    item_price = float(inv.inventory_dict[user_selection][0])
-    if item_price > money:
-        print("Error, insufficient funds")
-    elif item_price == money:
-        print(f'Here is your {user_selection} no change is required')
-        money = money - item_price
-        result = input("Would you like anything else: Yes/No ")
+    if inv.inventory_dict[user_selection][1] == 0:
+       print("Item is no longer availible, please choose again")
+       print("These are your choices:", inv.inventory_dict)
+       user_selection = io.get_choice(inv.inventory_dict)
     else:
-        change = money - item_price
-        print(f'Here is your {user_selection} and your change ${change}')
-        money = money - item_price
-        result = input("Would you like anything else: Yes/No ")  
+        item_price = float(inv.inventory_dict[user_selection][0])
+        if item_price > money:
+            print("Error, insufficient funds")
+        elif item_price == money:
+            print(f'Here is your {user_selection} no change is required')
+            money = money - item_price
+            result = input("Would you like anything else: Yes/No ")
+            inv.inventory_dict[user_selection][1] = inv.inventory_dict[user_selection][1] - 1
+        else:
+            change = money - item_price
+            print(f'Here is your {user_selection} and your change ${change}')
+            money = money - item_price
+            inv.inventory_dict[user_selection][1] = inv.inventory_dict[user_selection][1] - 1
+            result = input("Would you like anything else: Yes/No ") 
 main()
-
